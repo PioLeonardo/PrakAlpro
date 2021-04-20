5f831920fc5c86bde1df4ec8bab5efd3ee20a138
PioLeonardo <78341454+PioLeonardo@users.noreply.github.com>
2021-04-20 23:39:53 +0700

    Add files via upload

---
+++ 71200531_Tugas Membuat Video 8.py
@@ -0,0 +1,22 @@
+#Sujo diminta untuk membuat program yang dapat mendata mahasiswa yang ada di
+#2 kelas. Kelas A terdapat 5 orang dan Kelas B terdapat 4 orang.
+#============================================================================
+#input :kelas_a,kelas_b
+#proses :menggabungkan kelas a dan kelas b
+#output :mencetak Kelas a dan kelas b
+#============================================================================
+
+kelas_a = ['Pio','Supri','Sujo','Michael','Jarjit']
+kelas_b = ['Santo','Santi','Dusi','Kaido']
+
+print("Kelas A :")
+print("1.",kelas_a[0])
+print("2.",kelas_a[1])
+print("3.",kelas_a[2])
+print("4.",kelas_a[3])
+print("5.",kelas_a[4])
+print("Kelas B :")
+print("1.",kelas_b[0])
+print("2.",kelas_b[1])
+print("3.",kelas_b[2])
+print("4.",kelas_b[3])
